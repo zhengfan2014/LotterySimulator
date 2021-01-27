3c96d96cf97df09d4f1bc3817774d55e33f20ed9
zhengfan2014 <zhengfan2014@outlook.com>
2021-01-27 23:08:06 +0800

    test

--- js/public.js
+++ js/public.js
@@ -1,3 +1,4 @@
 const JSONList = [
+	{ id: "01", name: '「奔行世间」常驻祈愿 - 阿贝多100% UP', describe: '', file: "xiaochou.json" },
 	{ id: "01", name: '「奔行世间」常驻祈愿', describe: '奔行世间」常驻祈愿为永久性祈愿活动，玩家可以抽取非限定角色与武器。', file: "yuanshen.json" }
 ]
